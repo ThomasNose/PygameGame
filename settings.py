44ea789b2ec4f47fb21c5c7c4a8ed7de067897ea
ThomasNose <thomasadugdale@hotmail.com>
2021-10-24 18:18:59 +0100

    Add files via upload
    Finally managed to get a loop working for adding the floor which I can alter to be any length I want. Works perfectly with my modified ground texture + allows for big highscores ;) 
    
    Alter the length of the floor by changing the FOR loop in "settings.py" to be X length (default is 100) .
    
    To add any actual platforms that aren't making up the ground you must append after the main loop in "settings.py".

--- settings.py
+++ settings.py
@@ -19,27 +19,40 @@ PLAYER_LIVES=3
 
 #starting platforms
 
-PLATFORM_LIST = [(0,HEIGHT - 40, 2*WIDTH, 40),
-                 (120,HEIGHT - 40, 2*WIDTH, 40),
-                 (300,HEIGHT - 40, 2*WIDTH, 40),
-                 (480,HEIGHT - 40, 2*WIDTH, 40),
-                 (660,HEIGHT - 40, 2*WIDTH, 40),
-                 (840,HEIGHT - 40, 2*WIDTH, 40),
-                 (1020,HEIGHT - 40, 2*WIDTH, 40),
-                 (1200,HEIGHT - 40, 2*WIDTH, 40),
-                 (1380,HEIGHT - 40, 2*WIDTH, 40),
-                 (1560,HEIGHT - 40, 2*WIDTH, 40),
-                 (1740,HEIGHT - 40, 2*WIDTH, 40),
-                 (1920,HEIGHT - 40, 2*WIDTH, 40),
-                 (2100,HEIGHT - 40, 2*WIDTH, 40),
-                 (2280,HEIGHT - 40, 2*WIDTH, 40),
-                 (2460,HEIGHT - 40, 2*WIDTH, 40),
-                 (2640,HEIGHT - 40, 2*WIDTH, 40),
-                 (2820,HEIGHT - 40, 2*WIDTH, 40),
-                 (3000,HEIGHT - 40, 2*WIDTH, 40),
-                 (1000,HEIGHT - 200, 2*WIDTH, 40),
-                 (1200,HEIGHT - 300, 2*WIDTH, 40),
-                 (1400,HEIGHT - 150, 2*WIDTH, 40)]
+
+
+#PLATFORM_LIST = [(0,HEIGHT - 40, 2*WIDTH, 40),
+#                 (120,HEIGHT - 40, 2*WIDTH, 40),
+#                 (300,HEIGHT - 40, 2*WIDTH, 40),
+#                 (480,HEIGHT - 40, 2*WIDTH, 40),
+#                 (660,HEIGHT - 40, 2*WIDTH, 40),
+#                 (840,HEIGHT - 40, 2*WIDTH, 40),
+#                 (1020,HEIGHT - 40, 2*WIDTH, 40),
+#                 (1200,HEIGHT - 40, 2*WIDTH, 40),
+#                 (1380,HEIGHT - 40, 2*WIDTH, 40),
+#                 (1560,HEIGHT - 40, 2*WIDTH, 40),
+#                 (1740,HEIGHT - 40, 2*WIDTH, 40),
+#                 (1920,HEIGHT - 40, 2*WIDTH, 40),
+#                 (2100,HEIGHT - 40, 2*WIDTH, 40),
+#                 (2280,HEIGHT - 40, 2*WIDTH, 40),
+#                 (2460,HEIGHT - 40, 2*WIDTH, 40),
+#                 (2640,HEIGHT - 40, 2*WIDTH, 40),
+#                 (2820,HEIGHT - 40, 2*WIDTH, 40),
+#                 (3000,HEIGHT - 40, 2*WIDTH, 40),
+#                 (1000,HEIGHT - 200, 2*WIDTH, 40),
+#                 (1200,HEIGHT - 300, 2*WIDTH, 40),
+#                 (1400,HEIGHT - 150, 2*WIDTH, 40)]
+
+
+#PLATFORM LIST
+PLATFORM_LIST = []
+TEST = []
+
+# THIS IS THE INITIAL PLATFORM + LOOP FOR MAKING THE FLOOR, MAKE A LIST OF APPENDS LATER ON FOR ACTUALLY ADDING OBSTACLES
+Platform = [(120,HEIGHT - 40, 2*WIDTH, 40)]
+for i in range(100):
+	    TEST = (Platform[0][0]*i,Platform[0][1],Platform[0][2],Platform[0][3])
+	    PLATFORM_LIST.append(TEST)
 
 FLOOR_LIST = [(0,HEIGHT - 40, 2*WIDTH, 40)]
 
